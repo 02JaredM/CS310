35520a0a6fc37937d7a54008eb9eccc474a549af
02JaredM <jaredpmyers2@gmail.com>
2025-09-08 00:17:44 +0000

    Lab: inheritance + exceptions + JSON/CSV/YAML + Catch2

---
+++ inheritance-serialization/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/CS310/inheritance-serialization
+# Build directory: /workspaces/CS310/inheritance-serialization/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/workspaces/CS310/inheritance-serialization/build/tests-b12d07c_include.cmake")
+subdirs("_deps/json-build")
+subdirs("_deps/yaml-cpp-build")
+subdirs("_deps/catch2-build")
